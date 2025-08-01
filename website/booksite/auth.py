ae5fd81f4ee42ea8a3724bf2d1802cd444f84662
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-08-01 11:35:23 +0530

    Update auth.py

--- website/booksite/auth.py
+++ website/booksite/auth.py
@@ -27,7 +27,7 @@ class LoginClassView(LoginView):
 
     def form_valid(self, form):
         messages.success(self.request, "Account logged in successfully")
-        return super(),form_valid(form)
+        return super().form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "Invalid credentials!")
