48869b2186baaf431e4a76b7ee6ab33b3b1e1289
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-08-01 11:31:36 +0530

    Update auth.py

--- website/booksite/auth.py
+++ website/booksite/auth.py
@@ -23,10 +23,11 @@ class SignUpView(CreateView):
 class LoginClassView(LoginView):
     template_name = "booksite/login.html"
     redirect_authenticated_user = True
-    
-    def get_success_url(self):
+    next_page = reverse_lazy('home')
+
+    def form_valid(self, form):
         messages.success(self.request, "Account logged in successfully")
-        return reverse_lazy('homme')
+        return super(),form_valid(form)
 
     def form_invalid(self, form):
         messages.error(self.request, "Invalid credentials!")
